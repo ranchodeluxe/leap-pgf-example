b46d3ff313cdfafb3e3c8f72b29f9e5af0af8769
Raphael Hagen <norlandrhagen@gmail.com>
2023-10-02 14:33:47 -0600

    updated meta.yaml url + naming

--- feedstock/recipe.py
+++ feedstock/recipe.py
@@ -1,3 +1,7 @@
+# This recipe can be run with `pangeo-forge-runner` with the CLI command:
+# pangeo-forge-runner bake --repo=~/Documents/carbonplan/leap-pgf-example/ -f ~/Documents/carbonplan/leap-pgf-example/feedstock/config.json --Bake.job_name=AGCD --Bake.job_name=agcd
+
+
 import apache_beam as beam
 from pangeo_forge_recipes.transforms import (
     OpenURLWithFSSpec,
@@ -9,18 +13,18 @@ from pangeo_forge_recipes.transforms import Indexed, T
 
 
 # Github url to meta.yml:
-meta_yml_url = "https://github.com/carbonplan/leap-pgf-example/blob/main/meta.yml"
+meta_yaml_url = (
+    "https://github.com/carbonplan/leap-pgf-example/blob/main/feedstock/meta.yaml"
+)
 
 # Filename Pattern Inputs
 target_chunks = {"time": 40}
 
 # Time Range
-years = list(range(1971, 1973))
-# years = list(range(1971, 2020))
+years = list(range(1971, 1973))  # 2020
 
 # Variable List
-variables = ["precip", "tmax"]
-# variables = ["precip", "tmax", "tmin", "vapourpres_h09", "vapourpres_h15"]
+variables = ["precip", "tmax"]  # "tmin", "vapourpres_h09", "vapourpres_h15"
 
 
 def make_filename(variable, time):
@@ -40,7 +44,7 @@ pattern = FilePattern(
 
 class DropVars(beam.PTransform):
     """
-    Drop vars
+    Custom Beam tranform to drop unused vars
     """
 
     @staticmethod
@@ -57,14 +61,11 @@ AGCD = (
     beam.Create(pattern.items())
     | OpenURLWithFSSpec()
     | OpenWithXarray(file_type=pattern.file_type)
-    | DropVars()  # New preprocessor
+    | DropVars()
     | StoreToZarr(
         store_name="AGCD.zarr",
         combine_dims=pattern.combine_dim_keys,
         target_chunks=target_chunks,
-        attrs={"meta_yml_url": meta_yml_url},
+        attrs={"meta_yaml_url": meta_yaml_url},
     )
 )
-
-
-# pangeo-forge-runner bake --repo=~/Documents/carbonplan/leap-pgf-example/ -f ~/Documents/carbonplan/leap-pgf-example/feedstock/config.json --Bake.job_name=AGCD --Bake.job_name=agcd
